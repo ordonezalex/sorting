95b52c7ac1f4284bb7485cbdbfae0bd286d72c87
Alex Ordonez <ordonez.alex.pierce@gmail.com>
2014-12-01 13:10:48 -0500

    Fix SelectionSort to use index 0

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,14 +1,13 @@
 public class SelectionSort implements Sorter {
 
     @Override
-    public <E> E[] sort(E[] unsorted) {
+    public <E extends Comparable<E>> E[] sort(E[] unsorted) {
 
-        for (int i = 1; i <= unsorted.length - 1; i++) {
-
+        for (int i = 0; i < unsorted.length - 1; i++) {
             int smallest = i;
 
-            for (int j = i + 1; j <= unsorted.length; j++) {
-                if (unsorted[j] < unsorted[smallest]) {
+            for (int j = i + 1; j < unsorted.length; j++) {
+                if (unsorted[j].compareTo(unsorted[smallest]) < 0) {
                     smallest = j;
                 }
 
