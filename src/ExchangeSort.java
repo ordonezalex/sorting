bd13939ae954f8bf531f437ad428b66405d4f212
Alex Ordonez <ordonez.alex.pierce@gmail.com>
2014-12-01 13:02:15 -0500

    Update ExchangeSort to use Comparable

--- src/ExchangeSort.java
+++ src/ExchangeSort.java
@@ -1,11 +1,11 @@
 public class ExchangeSort implements Sorter {
 
     @Override
-    public <E> E[] sort(E[] unsorted) {
+    public <E extends Comparable<E>> E[] sort(E[] unsorted) {
 
         for (int i = 1; i <= unsorted.length; i++) {
             for (int j = i + 1; j <= unsorted.length; j++) {
-                if (unsorted[j] < unsorted[i]) {
+                if (unsorted[i].compareTo(unsorted[j]) > 0) {
 
                     // Swap i and j
                     E temp = unsorted[i];
