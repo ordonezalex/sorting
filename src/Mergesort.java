5c9ca211ba2fb6ce8ecda61e38542ce569a5b47e
Alex Ordonez <ordonez.alex.pierce@gmail.com>
2014-12-01 13:12:28 -0500

    Update Mergesort to use Comparable

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -3,7 +3,7 @@ import java.lang.reflect.Array;
 public class Mergesort implements Sorter {
 
     @Override
-    public <E> E[] sort(E[] unsorted) {
+    public <E extends Comparable<E>> E[] sort(E[] unsorted) {
 
         if (unsorted.length > 1) {
             int h = (int) Math.floor(unsorted.length / 2), m = unsorted.length - h;
